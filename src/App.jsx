e681f9be5971cbb86f94d0b89e692327f2f916bf
Mohimkhan <mohimkhan652@gmail.com>
2026-04-20 19:53:36 +0600

    chore(App.jsx): remove unecessary imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
 import ZeroToMillion from "./zero_to_million";
 
 const App = () => {
